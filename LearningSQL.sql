822e63913fd87fd757c9654dca097d5572f7e601
Anushka1-Singh <9651anushka@gmail.com>
2026-01-20 15:56:37 +0530

    added

--- LearningSQL.sql
+++ LearningSQL.sql
@@ -59,5 +59,37 @@ select * from accounts;
 
 
 
+-- (joins), joins are used to combine rows from two or more tables based on a related column between them.
+create table customers(customer_id int primary key, name varchar(50), city varchar(50));
+insert into customers values(1,'alice','mumbai'),(2,'bob','delhi'),(3,'charlie','banglore'),
+(4,'david','mumbai');
+create table orders(order_id int primary key, customer_id int, amount int);
+insert into orders values(101, 1, 500),(102, 1, 900),(103, 2, 300),(104, 5, 700);
+show tables;
+select * from customers;
+select * from orders;
 
+-- inner join
+select * from customers c 
+inner join orders o on c.customer_id = o.customer_id;
+select c.customer_id, o.order_id, c.name from customers c 
+inner join orders o on c.customer_id = o.customer_id;
+-- left join 
+select * from customers c 
+left join orders o on c.customer_id = o.customer_id;
+-- right join
+select * from customers c 
+right join orders o on c.customer_id = o.customer_id;
+-- outer join 
+select * from customers c 
+left join orders o on c.customer_id = o.customer_id
+union
+select * from customers c 
+right join orders o on c.customer_id = o.customer_id;
+-- cross join
+select * from customers cross join orders;
+-- self join ( it is a regular join but the table is jined with itself)
+select * from customers as A join customers as B on A.customer_id = B.customer_id;
+
+-- excusive joins (left exclusive and right exclusive join)
 
